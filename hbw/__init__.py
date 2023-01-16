ab45a1c6a97d21a8ac8b8f77e53a16b27961414f
Mathis Frahm <mathisfrahm@gmx.de>
2023-01-16 17:05:10 +0100

    make importing of config object easier

---
+++ hbw/__init__.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+# flake8: noqa
+
+
+__all__ = [
+    "analysis_hbw", "config_2017", "config_2017_limited",
+]
+
+# provisioning imports
+from hbw.config.analysis_hbw import analysis_hbw, config_2017, config_2017_limited
