2d33e8237125d562b6fa6d582dfbc84158860553
Mathis Frahm <mathisfrahm@gmx.de>
2023-01-16 17:18:27 +0100

    implement functions for new categories

--- hbw/selection/categories.py
+++ hbw/selection/categories.py
@@ -4,6 +4,8 @@
 Selection methods defining categories based on selection step results.
 """
 
+import functools
+
 from columnflow.util import maybe_import
 from columnflow.selection import Selector, SelectionResult, selector
 
@@ -12,15 +14,88 @@ ak = maybe_import("awkward")
 
 
 @selector(uses={"event"})
-def sel_incl(self: Selector, events: ak.Array, results: SelectionResult, **kwargs) -> ak.Array:
+def catid_selection_incl(self: Selector, events: ak.Array, **kwargs) -> ak.Array:
     return ak.ones_like(events.event)
 
 
 @selector(uses={"event"})
-def sel_1e(self: Selector, events: ak.Array, results: SelectionResult, **kwargs) -> ak.Array:
+def catid_selection_1e(self: Selector, events: ak.Array, results: SelectionResult, **kwargs) -> ak.Array:
     return (ak.num(results.objects.Electron.Electron, axis=-1) == 1) & (ak.num(results.objects.Muon.Muon, axis=-1) == 0)
 
 
 @selector(uses={"event"})
-def sel_1mu(self: Selector, events: ak.Array, results: SelectionResult, **kwargs) -> ak.Array:
+def catid_selection_1mu(self: Selector, events: ak.Array, results: SelectionResult, **kwargs) -> ak.Array:
     return (ak.num(results.objects.Electron.Electron, axis=-1) == 0) & (ak.num(results.objects.Muon.Muon, axis=-1) == 1)
+
+
+@selector(uses={"Electron.pt", "Muon.pt"})
+def catid_1e(self: Selector, events: ak.Array, **kwargs) -> ak.Array:
+    return (ak.num(events.Electron.pt, axis=-1) == 1) & (ak.num(events.Muon.pt, axis=-1) == 0)
+
+
+@selector(uses={"Electron.pt", "Muon.pt"})
+def catid_1mu(self: Selector, events: ak.Array, **kwargs) -> ak.Array:
+    return (ak.num(events.Electron.pt, axis=-1) == 0) & (ak.num(events.Muon.pt, axis=-1) == 1)
+
+
+@selector(uses={"Jet.pt", "FatJet.pt"})
+def catid_boosted(self: Selector, events: ak.Array, **kwargs) -> ak.Array:
+    """
+    Categorization of events in the boosted category: presence of at least 1 AK8 jet fulfilling
+    requirements given by the Selector called in SelectEvents
+    """
+    return (ak.num(events.Jet.pt, axis=-1) >= 1) & (ak.num(events.FatJet.pt, axis=-1) >= 1)
+
+
+@selector(uses={"Jet.pt", "FatJet.pt"})
+def catid_resolved(self: Selector, events: ak.Array, **kwargs) -> ak.Array:
+    """
+    Categorization of events in the resolved category: presence of no AK8 jets fulfilling
+    requirements given by the Selector called in SelectEvents
+    """
+    return (ak.num(events.Jet.pt, axis=-1) >= 3) & (ak.num(events.FatJet.pt, axis=-1) == 0)
+
+
+@selector(uses={catid_resolved, "Jet.btagDeepFlavB"})
+def catid_resolved_1b(self: Selector, events: ak.Array, **kwargs) -> ak.Array:
+
+    n_deepjet = ak.sum(events.Jet.btagDeepFlav >= self.config_inst.x.btag_working_points.deepjet.medium)
+
+    return catid_resolved & (n_deepjet == 1)
+
+
+@selector(uses={catid_resolved, "Jet.btagDeepFlavB"})
+def catid_resolved_2b(self: Selector, events: ak.Array, **kwargs) -> ak.Array:
+
+    n_deepjet = ak.sum(events.Jet.btagDeepFlav >= self.config_inst.x.btag_working_points.deepjet.medium)
+
+    return catid_resolved & (n_deepjet >= 2)
+
+
+for lep_ch in ["1e", "1mu"]:
+    for jet_ch in ["resolved_1b", "resolved_2b", "boosted"]:
+        for dnn_ch in ["dummy"]:
+
+            funcs = {
+                "lep_func": locals()[f"catid_{lep_ch}"],
+                "jet_func": locals()[f"catid_{jet_ch}"],
+                # "dnn_func": locals()[f"catid_{dnn_ch}"],
+            }
+
+            # @selector(name=f"catid_{lep_ch}_{jet_ch}")
+            @selector(
+                uses=set(funcs.values()),
+                produces=set(funcs.values()),
+                cls_name=f"catid_{lep_ch}_{jet_ch}"
+            )
+            def catid_leaf_mask(self: Selector, events: ak.Array, **kwargs) -> ak.Array:
+                """
+                Selector that call multiple functions and combines their outputs via
+                logical `and`.
+                """
+                # masks = [func(self, events, **kwargs) for func in funcs]
+                masks = [self[func](events, **kwargs) for func in self.uses]
+
+                leaf_mask = functools.reduce((lambda a, b: a & b), masks)
+
+                return leaf_mask
