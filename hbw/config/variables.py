82bc4fa1af18e2eda2b7399cd143330ffd62f169
Mathis Frahm <mathisfrahm@gmx.de>
2023-03-14 13:36:12 +0100

    add default variables

--- hbw/config/variables.py
+++ hbw/config/variables.py
@@ -102,6 +102,31 @@ def add_variables(config: od.Config) -> None:
     Adds all variables to a *config* that are present after `ReduceEvents`
     without calling any producer
     """
+
+    # (the "event", "run" and "lumi" variables are required for some cutflow plotting task,
+    # and also correspond to the minimal set of columns that coffea's nano scheme requires)
+    config.add_variable(
+        name="event",
+        expression="event",
+        binning=(1, 0.0, 1.0e9),
+        x_title="Event number",
+    discrete_x=True,
+    )
+    config.add_variable(
+        name="run",
+        expression="run",
+        binning=(1, 100000.0, 500000.0),
+        x_title="Run number",
+        discrete_x=True,
+    )
+    config.add_variable(
+        name="lumi",
+        expression="luminosityBlock",
+        binning=(1, 0.0, 5000.0),
+        x_title="Luminosity block",
+        discrete_x=True,
+    )
+
     #
     # Weights
     #
