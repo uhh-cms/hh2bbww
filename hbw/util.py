ff4ecee81f47a51bb2fc43f3a9be835f4331a496
Mathis Frahm <mathisfrahm@gmx.de>
2023-06-04 10:42:41 +0200

    implement util fuctions for development

--- hbw/util.py
+++ hbw/util.py
@@ -10,10 +10,18 @@ import tracemalloc
 
 import law
 
-logger = law.logger.get_logger(__name__)
+_logger = law.logger.get_logger(__name__)
 
 
-def log_memory(message: str = "", unit: str = "MB", restart: bool = False):
+def log_memory(
+    message: str = "",
+    unit: str = "MB",
+    restart: bool = False,
+    logger=None,
+):
+    if logger is None:
+        logger = _logger
+
     if restart or not tracemalloc.is_tracing():
         logger.info("Start tracing memory")
         tracemalloc.start()
@@ -35,3 +43,29 @@ def memory_GB(y_true, y_pred):
         tracemalloc.start()
 
     return tracemalloc.get_traced_memory()[0] / 1024 ** 3
+
+
+def debugger():
+    """
+    Small helper to give some more information when starting IPython.embed
+    """
+
+    # get the previous frame to get some infos
+    frame = __import__("inspect").currentframe().f_back
+
+    header = (
+        f"Line: {frame.f_lineno}, Function: {frame.f_code.co_name}, "
+        f"File: {frame.f_code.co_filename}"
+    )
+
+    # get the namespace of the previous frame
+    namespace = frame.f_globals.copy()
+    namespace.update(frame.f_locals)
+
+    # add helper function to namespace
+    namespace.update({
+        "q": __import__("functools").partial(__import__("os")._exit, 0),
+    })
+
+    # start the debugger
+    __import__("IPython").embed(header=header, user_ns=namespace)
