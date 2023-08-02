e44898d2b7445e866428f2146cbf197d1a8eb1f3
Lara <lara.markus@gmx.de>
2023-08-02 15:15:14 +0200

    removed open_parquet.py

--- open_parquet.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-import awkward as ak
-events = ak.from_parquet("/nfs/dust/cms/user/markusla/analysis/dilepton/hh2bbww/data/hbw_store/hbw_dl/cf.ReduceEvents/l17/tt_dl_powheg/nominal/calib__skip_jecunc/sel__dl/v1/")
-
-from IPython import embed; embed()
