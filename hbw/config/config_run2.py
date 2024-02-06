93266cae53f5a2abb0426196566a6fbc30f3177d
Mathis Frahm <mathisfrahm@gmx.de>
2024-02-06 09:49:48 +0100

    update external files

--- hbw/config/config_run2.py
+++ hbw/config/config_run2.py
@@ -139,14 +139,14 @@ def add_config(
     jerc_postfix = ""
     if year == 2016 and campaign.x.vfp == "post":
         jerc_postfix = "APV"
-    elif year == 2022:
-        jerc_postfix = f"{campaign.x.EE}EE"
+    elif year == 2022 and campaign.x.EE == "post":
+        jerc_postfix = "EE"
 
     if cfg.x.run == 2:
         jerc_campaign = f"Summer19UL{year2}{jerc_postfix}"
         jet_type = "AK4PFchs"
     elif cfg.x.run == 3:
-        jerc_campaign = f"Winter{year2}Run3"
+        jerc_campaign = f"Summer{year2}{jerc_postfix}_22Sep2023"
         jet_type = "AK4PFPuppi"
 
     cfg.x.jec = DotDict.wrap({
@@ -219,8 +219,8 @@ def add_config(
     # https://twiki.cern.ch/twiki/bin/view/CMS/JetResolution?rev=107
     # TODO: get jerc working for Run3
     cfg.x.jer = DotDict.wrap({
-        "campaign": jerc_campaign if cfg.x.run == 2 else f"JR_{jerc_campaign}",
-        "version": {2016: "JRV3", 2017: "JRV2", 2018: "JRV2", 2022: "V1"}[year],
+        "campaign": jerc_campaign,
+        "version": {2016: "JRV3", 2017: "JRV2", 2018: "JRV2", 2022: "V2"}[year],
         "jet_type": jet_type,
     })
 
@@ -409,11 +409,13 @@ def add_config(
         json_mirror = "/afs/cern.ch/user/m/mrieger/public/mirrors/jsonpog-integration-9ea86c4c"
         corr_tag = f"{cfg.x.cpn_tag}_UL"
     elif cfg.x.run == 3:
-        # TODO: Update when possible
-        json_mirror = "/afs/desy.de/user/p/paaschal/public/mirrors/jsonpog-integration"
-        corr_tag = f"{year}_Prompt"
+        json_mirror = "/nfs/dust/cms/user/frahmmat/public/mirrors/jsonpog-integration-f35ab53e"
+        corr_tag = f"{year}_Summer22{jerc_postfix}"
 
     cfg.x.external_files = DotDict.wrap({
+        # pileup weight corrections
+        "pu_sf": (f"{json_mirror}/POG/LUM/{corr_tag}/puWeights.json.gz", "v1"),
+
         # jet energy correction
         "jet_jerc": (f"{json_mirror}/POG/JME/{corr_tag}/jet_jerc.json.gz", "v1"),
 
@@ -431,21 +433,20 @@ def add_config(
     })
 
     # temporary fix due to missing corrections in run 3
+    # electron, muon and met still missing, btag, and pu are TODO
     if cfg.x.run == 3:
-        cfg.add_tag("skip_btag_weights")
-        cfg.add_tag("skip_electron_weights")
-        cfg.add_tag("skip_muon_weights")
         cfg.add_tag("skip_pu_weights")
-        cfg.x.external_files.pop("electron_sf")
-        cfg.x.external_files.pop("muon_sf")
-        cfg.x.external_files.pop("btag_sf_corr")
-        cfg.x.external_files.pop("met_phi_corr")
 
-        # NOTE: we should mirror and gzip them
-        if campaign.x.EE == "pre":
-            cfg.x.external_files["muon_sf"] = ("https://gitlab.cern.ch/cms-muonPOG/muonefficiencies/-/raw/master/Run3/2022/2022_Z/ScaleFactors_Muon_Z_ID_ISO_2022_schemaV2.json", "v1")  # noqa
-        elif campaign.x.EE == "post":
-            cfg.x.external_files["muon_sf"] = ("https://gitlab.cern.ch/cms-muonPOG/muonefficiencies/-/raw/master/Run3/2022_EE/2022_Z/ScaleFactors_Muon_Z_ID_ISO_2022_EE_schemaV2.json", "v1")  # noqa
+        cfg.add_tag("skip_btag_weights")
+        cfg.x.external_files.pop("btag_sf_corr")
+
+        cfg.add_tag("skip_electron_weights")
+        cfg.x.external_files.pop("electron_sf")
+
+        cfg.add_tag("skip_muon_weights")
+        cfg.x.external_files.pop("muon_sf")
+
+        cfg.x.external_files.pop("met_phi_corr")
 
     cfg.x.met_filters = {
         "Flag.goodVertices",
