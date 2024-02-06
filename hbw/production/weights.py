b06a1ca1f8e24cc91c4a0fa19acd2cbc8383be17
Mathis Frahm <mathisfrahm@gmx.de>
2024-02-06 13:08:24 +0100

    implement and use custom pileup weight producer via correctionlib

--- hbw/production/weights.py
+++ hbw/production/weights.py
@@ -8,7 +8,9 @@ from columnflow.util import maybe_import
 from columnflow.columnar_util import set_ak_column, has_ak_column, Route
 from columnflow.selection import SelectionResult
 from columnflow.production import Producer, producer
-from columnflow.production.cms.pileup import pu_weight
+from hbw.production.pileup import pu_weight_from_correctionlib
+# TODO: switch to columnflow Producer as soon as PR is merged
+# from columnflow.production.cms.pileup import pu_weight  # , pu_weight_from_correctionlib
 from columnflow.production.normalization import normalization_weights
 from columnflow.production.cms.electron import electron_weights
 from columnflow.production.cms.muon import muon_weights
@@ -60,8 +62,8 @@ def event_weight_init(self: Producer) -> None:
 
 
 @producer(
-    uses={pu_weight},
-    produces={pu_weight},
+    uses={pu_weight_from_correctionlib},
+    produces={pu_weight_from_correctionlib},
     mc_only=True,
 )
 def event_weights_to_normalize(self: Producer, events: ak.Array, results: SelectionResult, **kwargs) -> ak.Array:
@@ -71,7 +73,7 @@ def event_weights_to_normalize(self: Producer, events: ak.Array, results: Select
     """
 
     # compute pu weights
-    events = self[pu_weight](events, **kwargs)
+    events = self[pu_weight_from_correctionlib](events, **kwargs)
 
     if not has_tag("skip_btag_weights", self.config_inst, self.dataset_inst, operator=any):
         # compute btag SF weights (for renormalization tasks)
@@ -127,7 +129,7 @@ normalized_pdf_weights = normalized_weight_factory(
 
 normalized_pu_weights = normalized_weight_factory(
     producer_name="normalized_pu_weights",
-    weight_producers={pu_weight},
+    weight_producers={pu_weight_from_correctionlib},
 )
 
 
