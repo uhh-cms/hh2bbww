163385235f0e37fe670739a1191f5448a5ae18d1
Mathis Frahm <mathisfrahm@gmx.de>
2024-02-29 13:38:42 +0100

    add lt variable and angles between W and lepton

--- hbw/production/ml_inputs.py
+++ hbw/production/ml_inputs.py
@@ -75,6 +75,7 @@ def ml_inputs(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
 
     # jets in general
     events = set_ak_column_f32(events, "mli_ht", ak.sum(events.Jet.pt, axis=1))
+    events = set_ak_column_f32(events, "mli_lt", ak.sum(events.Lepton.pt, axis=1) + events.MET.pt)
     events = set_ak_column_f32(events, "mli_n_jet", ak.num(events.Jet.pt, axis=1))
 
     # all possible jet pairs
@@ -89,6 +90,7 @@ def ml_inputs(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
     events = set_ak_column_f32(events, "mli_vbf_tag", vbf_tag)
 
     # bjets in general
+    # TODO: generalize using selection
     wp_med = self.config_inst.x.btag_working_points.deepjet.medium
     events = set_ak_column_f32(events, "mli_n_deepjet", ak.num(events.Jet[events.Jet.btagDeepFlavB > wp_med], axis=1))
     events = set_ak_column_f32(events, "mli_deepjetsum", ak.sum(events.Jet.btagDeepFlavB, axis=1))
@@ -102,9 +104,6 @@ def ml_inputs(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
     hbb = events.Bjet[:, 0] + events.Bjet[:, 1]
     events = set_ak_column_f32(events, "mli_mbb", hbb.mass)
 
-    mindr_lb = ak.min(events.Bjet.delta_r(events.Lepton[:, 0]), axis=-1)
-    events = set_ak_column_f32(events, "mli_mindr_lb", mindr_lb)
-
     # wjj features
     events = set_ak_column_f32(events, "mli_dr_jj", events.Lightjet[:, 0].delta_r(events.Lightjet[:, 1]))
     events = set_ak_column_f32(events, "mli_dphi_jj", abs(events.Lightjet[:, 0].delta_phi(events.Lightjet[:, 1])))
@@ -112,14 +111,19 @@ def ml_inputs(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
     wjj = events.Lightjet[:, 0] + events.Lightjet[:, 1]
     events = set_ak_column_f32(events, "mli_mjj", wjj.mass)
 
+    # wlnu features
+    # NOTE: we might want to consider neutrino reconstruction or transverse masses instead when including MET
+    wlnu = events.MET + events.Lepton[:, 0]
+    events = set_ak_column_f32(events, "mli_mlnu", wlnu.mass)
+    events = set_ak_column_f32(events, "mli_dphi_lnu", abs(events.Lepton[:, 0].delta_phi(events.MET)))
+    events = set_ak_column_f32(events, "mli_dphi_wl", abs(wlnu.delta_phi(events.Lepton[:, 0])))
+
+    # angles to lepton
+    mindr_lb = ak.min(events.Bjet.delta_r(events.Lepton[:, 0]), axis=-1)
+    events = set_ak_column_f32(events, "mli_mindr_lb", mindr_lb)
+
     mindr_lj = ak.min(events.Lightjet.delta_r(events.Lepton[:, 0]), axis=-1)
     events = set_ak_column_f32(events, "mli_mindr_lj", mindr_lj)
-
-    # wlnu features
-    wlnu = events.MET + events.Lepton[:, 0]
-    events = set_ak_column_f32(events, "mli_dphi_lnu", abs(events.Lepton[:, 0].delta_phi(events.MET)))
-    # NOTE: this column can be set to nan value
-    events = set_ak_column_f32(events, "mli_mlnu", wlnu.mass)
 
     # hww features
     hww = wlnu + wjj
@@ -128,7 +132,7 @@ def ml_inputs(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
     events = set_ak_column_f32(events, "mli_mjjlnu", hww.mass)
     events = set_ak_column_f32(events, "mli_mjjl", hww_vis.mass)
 
-    # angles
+    # hh system angles
     events = set_ak_column_f32(events, "mli_dphi_bb_jjlnu", abs(hbb.delta_phi(hww)))
     events = set_ak_column_f32(events, "mli_dr_bb_jjlnu", hbb.delta_r(hww))
 
@@ -137,8 +141,8 @@ def ml_inputs(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
 
     events = set_ak_column_f32(events, "mli_dphi_bb_nu", abs(hbb.delta_phi(events.MET)))
     events = set_ak_column_f32(events, "mli_dphi_jj_nu", abs(wjj.delta_phi(events.MET)))
-    events = set_ak_column_f32(events, "mli_dr_bb_l", hbb.delta_r(events.MET))
-    events = set_ak_column_f32(events, "mli_dr_jj_l", hbb.delta_r(events.MET))
+    events = set_ak_column_f32(events, "mli_dr_bb_l", hbb.delta_r(events.Lepton[:, 0]))
+    events = set_ak_column_f32(events, "mli_dr_jj_l", hbb.delta_r(events.Lepton[:, 0]))
 
     # hh features
     hh = hbb + hww
@@ -163,13 +167,27 @@ def ml_inputs(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
 @ml_inputs.init
 def ml_inputs_init(self: Producer) -> None:
     # define ML input separately to self.produces
-    self.ml_columns = {
-        "mli_ht", "mli_n_jet", "mli_n_deepjet",
+    self.config_inst.x.ml_columns = self.ml_columns = {
+        # event features
+        "mli_ht", "mli_lt", "mli_n_jet", "mli_n_deepjet",
         "mli_deepjetsum", "mli_b_deepjetsum", "mli_l_deepjetsum",
-        "mli_dr_bb", "mli_dphi_bb", "mli_mbb", "mli_mindr_lb", "mli_dr_jj", "mli_dphi_jj", "mli_mjj", "mli_mindr_lj",
-        "mli_dphi_lnu", "mli_mlnu", "mli_mjjlnu", "mli_mjjl", "mli_dphi_bb_jjlnu", "mli_dr_bb_jjlnu",
+        # bb system
+        "mli_dr_bb", "mli_dphi_bb", "mli_mbb",
+        # jj system
+        "mli_dr_jj", "mli_dphi_jj", "mli_mjj",
+        # lnu system
+        "mli_dphi_lnu", "mli_mlnu",
+        # angles to lepton
+        "mli_mindr_lb", "mli_mindr_lj",
+        "mli_dphi_wl",
+        # ww system
+        "mli_mjjlnu", "mli_mjjl",
+        # HH system
+        "mli_dphi_bb_jjlnu", "mli_dr_bb_jjlnu",
         "mli_dphi_bb_jjl", "mli_dr_bb_jjl", "mli_dphi_bb_nu", "mli_dphi_jj_nu", "mli_dr_bb_l", "mli_dr_jj_l",
-        "mli_mbbjjlnu", "mli_mbbjjl", "mli_s_min", "mli_mindr_jj",
+        "mli_mbbjjlnu", "mli_mbbjjl", "mli_mindr_jj",
+        "mli_s_min",
+        # VBF features
         "mli_vbf_deta", "mli_vbf_invmass", "mli_vbf_tag",
     } | set(
         f"mli_{obj}_{var}"
