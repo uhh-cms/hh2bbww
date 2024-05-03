c26dd4bce29c3b9379b814cb0adcfe59f08ba124
Mathis Frahm <mathisfrahm@gmx.de>
2024-05-03 10:52:04 +0200

    remove unnecessary requires in MLTraining

--- hbw/columnflow_patches.py
+++ hbw/columnflow_patches.py
@@ -8,11 +8,31 @@ import law
 from columnflow.util import memoize
 
 
+logger = law.logger.get_logger(__name__)
+
 @memoize
 def patch_mltraining():
+    logger.info("Patching MLTraining to use NestedSiblingFileCollection and remove unnecessary requires...")
     from columnflow.tasks.ml import MLTraining
+    from columnflow.tasks.framework.remote import RemoteWorkflow
 
+    # patch the MLTraining output collection
     MLTraining.output_collection_cls = law.NestedSiblingFileCollection
+
+    # remove unnceessary requires from MLTraining
+    def workflow_requires(self):
+        # cannot call super() here; hopefully we only need the workflow_requires from RemoteWorkflow
+        reqs = RemoteWorkflow.workflow_requires(self)
+        reqs["model"] = self.ml_model_inst.requires(self)
+        return reqs
+
+    def requires(self):
+        reqs = {}
+        reqs["model"] = self.ml_model_inst.requires(self)
+        return reqs
+
+    MLTraining.requires = requires
+    MLTraining.workflow_requires = workflow_requires
 
 
 @memoize
