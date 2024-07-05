7cedc2ae60392865afae333777e21f90b40ec75d
Mathis Frahm <mathisfrahm@gmx.de>
2024-07-05 08:40:31 +0200

    use histograms for btag reweighting

--- hbw/production/normalized_btag.py
+++ hbw/production/normalized_btag.py
@@ -13,6 +13,7 @@ from columnflow.columnar_util import set_ak_column
 
 np = maybe_import("numpy")
 ak = maybe_import("awkward")
+hist = maybe_import("hist")
 
 
 @producer(
@@ -23,6 +24,108 @@ ak = maybe_import("awkward")
     mc_only=True,
 )
 def normalized_btag_weights(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
+
+    variable_map = {
+        # NOTE: might be cleaner to use the ht and njet reconstructed during the selection (and also compare?)
+        "ht": ak.sum(events.Jet.pt, axis=1),
+        "n_jets": ak.num(events.Jet.pt, axis=1),
+    }
+
+    for weight_name in self[btag_weights].produces:
+        if not weight_name.startswith("btag_weight"):
+            continue
+
+        if weight_name not in self.sf_map:
+            raise KeyError(f"Missing scale factor for {weight_name}")
+
+        sf = self.sf_map[weight_name]
+        inputs = [variable_map[inp.name] for inp in sf.inputs]
+
+        norm_weight = sf.evaluate(*inputs)
+        norm_weight = norm_weight * events[weight_name]
+        events = set_ak_column(events, f"normalized_{weight_name}", norm_weight)
+
+    return events
+
+
+@normalized_btag_weights.init
+def normalized_btag_weights_init(self: Producer) -> None:
+    for weight_name in self[btag_weights].produces:
+        if not weight_name.startswith("btag_weight"):
+            continue
+        self.produces.add(f"normalized_{weight_name}")
+
+
+@normalized_btag_weights.requires
+def normalized_btag_weights_requires(self: Producer, reqs: dict) -> None:
+    from columnflow.tasks.selection import MergeSelectionStats
+    reqs["selection_stats"] = MergeSelectionStats.req(
+        self.task,
+        tree_index=0,
+        branch=-1,
+        _exclude=MergeSelectionStats.exclude_params_forest_merge,
+    )
+
+
+@normalized_btag_weights.setup
+def normalized_btag_weights_setup(self: Producer, reqs: dict, inputs: dict, reader_targets: InsertableDict) -> None:
+    """
+    Setup function for the normalized btag weights producer, which extracts the normalization factors
+    from the selection histograms. Histograms are identified via key following the naming format
+    `sum_mc_weight_btag_weight_{weight_name}_per_process_ht_njet`.
+    """
+    # load the selection hists
+    hists = inputs["selection_stats"]["collection"][0]["hists"].load(formatter="pickle")
+
+    # initialize the scale factor map
+    self.sf_map = {}
+
+    den = hists["sum_mc_weight_per_process_ht_njet"][{"process": sum, "steps": "selected_no_bjet"}].values()
+
+    for key in hists.keys():
+        if not key.startswith("sum_mc_weight_btag_weight") or not key.endswith("_per_process_ht_njet"):
+            continue
+
+        # extract the weight name
+        weight_name = key.replace("sum_mc_weight_", "").replace("_per_process_ht_njet", "")
+
+        # create the scale factor histogram
+        h = hists[key][{"process": sum}]
+
+        num = h[{"steps": "selected_no_bjet"}].values()
+
+        # calculate the scale factor and store it as a correctionlib evaluator
+        sf = np.where(
+            (num > 0) & (den > 0),
+            num / den,
+            1.0,
+        )
+
+        sfhist = hist.Hist(*h.axes[1:], data=sf)
+        sfhist.name = f"{weight_name}_renormalization"
+        sfhist.label = "out"
+
+        import correctionlib.convert
+        btag_renormalization = correctionlib.convert.from_histogram(sfhist)
+        btag_renormalization.description = f"{weight_name} re-normalization"
+
+        # set overflow bins behavior (default is to raise an error when out of bounds)
+        # NOTE: claming seems to not work for int axes. Hopefully the number of jets considered to
+        # create these SFs is always large enough to not hit the overflow bin.
+        btag_renormalization.data.flow = "clamp"
+
+        # store the evaluator
+        self.sf_map[weight_name] = btag_renormalization.to_evaluator()
+
+
+@producer(
+    uses={
+        btag_weights.PRODUCES, "process_id", "Jet.pt",
+    },
+    # produced columns are defined in the init function below
+    mc_only=True,
+)
+def normalized_btag_weights_from_json(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
 
     for weight_name in self[btag_weights].produces:
         if not weight_name.startswith("btag_weight"):
@@ -53,8 +156,8 @@ def normalized_btag_weights(self: Producer, events: ak.Array, **kwargs) -> ak.Ar
     return events
 
 
-@normalized_btag_weights.init
-def normalized_btag_weights_init(self: Producer) -> None:
+@normalized_btag_weights_from_json.init
+def normalized_btag_weights_from_json_init(self: Producer) -> None:
     for weight_name in self[btag_weights].produces:
         if not weight_name.startswith("btag_weight"):
             continue
@@ -63,8 +166,8 @@ def normalized_btag_weights_init(self: Producer) -> None:
         self.produces.add(f"normalized_njet_{weight_name}")
 
 
-@normalized_btag_weights.requires
-def normalized_btag_weights_requires(self: Producer, reqs: dict) -> None:
+@normalized_btag_weights_from_json.requires
+def normalized_btag_weights_from_json_requires(self: Producer, reqs: dict) -> None:
     from columnflow.tasks.selection import MergeSelectionStats
     reqs["selection_stats"] = MergeSelectionStats.req(
         self.task,
@@ -74,8 +177,10 @@ def normalized_btag_weights_requires(self: Producer, reqs: dict) -> None:
     )
 
 
-@normalized_btag_weights.setup
-def normalized_btag_weights_setup(self: Producer, reqs: dict, inputs: dict, reader_targets: InsertableDict) -> None:
+@normalized_btag_weights_from_json.setup
+def normalized_btag_weights_from_json_setup(
+    self: Producer, reqs: dict, inputs: dict, reader_targets: InsertableDict,
+) -> None:
     # load the selection stats
     stats = inputs["selection_stats"]["collection"][0]["stats"].load(formatter="json")
 
