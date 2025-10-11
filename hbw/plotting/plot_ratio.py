c61c87b2a40a64aa07a85ba1e2304940f5755ffe
Mathis Frahm <mathis.frahm@uni-hamburg.de>
2025-10-11 13:58:04 +0200

    (WIP) update plot_ratio plot func

--- hbw/plotting/plot_ratio.py
+++ hbw/plotting/plot_ratio.py
@@ -11,6 +11,7 @@ __all__ = []
 from collections import OrderedDict
 
 import law
+import order as od
 
 from columnflow.util import maybe_import
 from columnflow.plotting.plot_all import plot_all
@@ -26,13 +27,12 @@ from columnflow.plotting.plot_util import (
     remove_negative_contributions,
 )
 
+from columnflow.types import TYPE_CHECKING
 
-hist = maybe_import("hist")
 np = maybe_import("numpy")
-mpl = maybe_import("matplotlib")
-plt = maybe_import("matplotlib.pyplot")
-mplhep = maybe_import("mplhep")
-od = maybe_import("order")
+if TYPE_CHECKING:
+    plt = maybe_import("matplotlib.pyplot")
+    hist = maybe_import("hist")
 
 
 logger = law.logger.get_logger(__name__)
@@ -95,7 +95,7 @@ def plot_ratio(
 
     # add ratio between processes
     norm = None
-    interal = None
+    integral = None
     # per default, use the mc_stack as denominator
     for key, config in plot_config.items():
         if key == "mc_stack":
@@ -107,7 +107,7 @@ def plot_ratio(
             # if there was no mc_stack, just use the first process as denominator
             norm = config["hist"].values()
             integral = sum(norm)
-        if not "line" in key:
+        if "line" not in key:
             continue
         config["ratio_kwargs"] = config["kwargs"].copy()
         if shape_norm:
@@ -139,10 +139,7 @@ def plot_ratio(
     style_config["cms_label_cfg"] = {"lumi": "59.8", "com": 13}
     style_config["rax_cfg"]["ylabel"] = "Ratio"
 
-
     return plot_all(plot_config, style_config, **kwargs)
-
-
 
 
 def plot_variable_efficiency_slice(
@@ -154,8 +151,9 @@ def plot_variable_efficiency_slice(
     style_config: dict | None = None,
     shape_norm: bool = True,
     cumsum_reverse: bool = True,
-    slice_down: complex | None = None,
-    slice_up: complex | None = None,
+    slice_list: list[complex] | None = [200j, 400j, 600j, 800j],
+    # slice_down: complex | None = None,
+    # slice_up: complex | None = None,
     **kwargs,
 ):
     """
@@ -174,17 +172,28 @@ def plot_variable_efficiency_slice(
         --remove-output 0,a,y --workers 4
 
     """
-    if len(variable_insts) == 2:
-        slice_variable_inst = variable_insts[0]
-        # use axis 1 for slicing
-        variable_insts = [variable_insts[1]]
-        logger.info(
-            f"Using variable '{slice_variable_inst.name}' for slicing and '{variable_insts[0].name}' for plotting."
-        )
-        for proc_inst, proc_hist in hists.items():
+    import matplotlib as mpl
+    import matplotlib.pyplot as plt
+    if len(variable_insts) != 2:
+        raise Exception("This plot function requires exactly two variables.")
+
+    slice_variable_inst = variable_insts[0]
+    # use axis 1 for slicing
+    variable_insts = [variable_insts[1]]
+    logger.info(
+        f"Using variable '{slice_variable_inst.name}' for slicing and '{variable_insts[0].name}' for plotting.",
+    )
+
+    # build one plot per slice range
+    figures = []
+    axs_combined = []
+    for i, slice_up in enumerate(slice_list[1:]):
+        slice_down = slice_list[i]
+        logger.info(f"Slicing {slice_variable_inst.name} in range [{slice_down}, {slice_up}]")
+        inp_hists = hists.copy()
+        for proc_inst, proc_hist in inp_hists.items():
             if slice_down is not None and slice_up is not None:
-                slice_range = (slice_down, slice_up)
-                proc_hist = proc_hist[{slice_variable_inst.name: slice(*slice_range)}]
+                proc_hist = proc_hist[{slice_variable_inst.name: slice(slice_down, slice_up)}]
 
             # sum over each individual slice bin (to get rid of over and underflow)
             # NOTE: there has to be a better way to do this with hist (but .project does not allow passing flow=False)
@@ -193,40 +202,76 @@ def plot_variable_efficiency_slice(
             out_hist = proc_hist[{slice_variable_inst.name: 0}]
             for bin_idx in range(1, len(proc_hist.axes[slice_variable_inst.name])):
                 out_hist += proc_hist[{slice_variable_inst.name: bin_idx}]
-            hists[proc_inst] = out_hist
+            inp_hists[proc_inst] = out_hist
 
-    for proc_inst, proc_hist in hists.items():
-        if cumsum_reverse:
-            proc_hist.values()[...] = np.cumsum(proc_hist.values()[..., ::-1], axis=-1)[..., ::-1]
-            shape_norm_func = kwargs.get("shape_norm_func", lambda h, shape_norm: h.values()[0] if shape_norm else 1)
-        else:
-            proc_hist.values()[...] = np.cumsum(proc_hist.values(), axis=-1)
-            shape_norm_func = kwargs.get("shape_norm_func", lambda h, shape_norm: h.values()[-1] if shape_norm else 1)
+        for proc_inst, proc_hist in inp_hists.items():
+            if cumsum_reverse:
+                proc_hist.values()[...] = np.cumsum(proc_hist.values()[..., ::-1], axis=-1)[..., ::-1]
+                shape_norm_func = kwargs.get(
+                    "shape_norm_func",
+                    lambda h, shape_norm: h.values()[0] if shape_norm else 1,
+                )
+            else:
+                proc_hist.values()[...] = np.cumsum(proc_hist.values(), axis=-1)
+                shape_norm_func = kwargs.get(
+                    "shape_norm_func",
+                    lambda h, shape_norm: h.values()[-1] if shape_norm else 1,
+                )
 
-    # custom style changes
-    cat_label = category_inst.label if category_inst else ""
-    default_style_config = {
-        "ax_cfg": {"ylabel": "Efficiency" if shape_norm else "Cumulative entries"},
-    }
-    if slice_up and slice_down:
-        if isinstance(slice_up, complex) and isinstance(slice_down, complex):
-            slice_up = int(slice_up.imag)
-            slice_down = int(slice_down.imag)
-        default_style_config["annotate_cfg"] = {
-            "text": cat_label + "\n" + f"$p_{{T}} \in [{slice_down}, {slice_up}]$"
+        # custom style changes
+        cat_label = category_inst.label if category_inst else ""
+        default_style_config = {
+            "ax_cfg": {"ylabel": "Efficiency" if shape_norm else "Cumulative entries"},
         }
+        if slice_up and slice_down:
+            if isinstance(slice_up, complex) and isinstance(slice_down, complex):
+                slice_up = int(slice_up.imag)
+                slice_down = int(slice_down.imag)
+            default_style_config["annotate_cfg"] = {
+                "text": cat_label + "\n" + rf"$p_{{T}} \in [{slice_down}, {slice_up}]$",
+            }
 
-    style_config = law.util.merge_dicts(default_style_config, style_config, deep=True)
+        style_config = law.util.merge_dicts(default_style_config, style_config, deep=True)
 
-    from columnflow.plotting.plot_functions_1d import plot_variable_stack
-    return plot_variable_stack(
-        hists,
-        config_inst,
-        category_inst,
-        variable_insts,
-        shift_insts,
-        shape_norm=shape_norm,
-        shape_norm_func=shape_norm_func,
-        style_config=style_config,
-        **kwargs,
-    )
+        from columnflow.plotting.plot_functions_1d import plot_variable_stack
+        fig, axs = plot_variable_stack(
+            inp_hists,
+            config_inst,
+            category_inst,
+            variable_insts,
+            shift_insts,
+            shape_norm=shape_norm,
+            shape_norm_func=shape_norm_func,
+            style_config=style_config,
+            **kwargs,
+        )
+        figures.append(fig)
+        axs_combined.append(axs)
+
+    # put all figures into a single large canvas
+    n_figs = len(figures)
+    n_cols = int(np.ceil(np.sqrt(n_figs)))
+    n_rows = int(np.ceil(n_figs / n_cols))
+    fig, axs = plt.subplots(n_rows, n_cols, figsize=(6 * n_cols, 5 * n_rows))
+    for i, (fig_i, axs_i) in enumerate(zip(figures, axs_combined)):
+        row = int(i / n_cols)
+        col = i % n_cols
+        if n_rows > 1 and n_cols > 1:
+            ax = axs[row, col]
+        elif n_rows > 1:
+            ax = axs[row]
+        elif n_cols > 1:
+            ax = axs[col]
+        else:
+            ax = axs
+        for child in fig_i.get_children():
+            if isinstance(child, mpl.axes.Axes):
+                for item in child.get_children():
+                    try:
+                        item.remove()
+                        item.set_figure(fig)
+                        ax.add_artist(item)
+                    except Exception:
+                        pass
+
+    return fig, axs
